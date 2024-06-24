9874c03dfdb5aa68b9d1e552decee29ac0f60fb9
Esan Taiwo <145122266+tmeechh@users.noreply.github.com>
2024-06-24 01:04:14 +0100

    ssss

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe"
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-const frontend_url = "http://localhost:5173"
+const frontend_url = "https://tmeech-food-delivery-app.vercel.app/"
 
 
 
