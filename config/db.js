a7bccca264c0ba01dbb89c64d7df8221ad886381
Esan Taiwo <145122266+tmeechh@users.noreply.github.com>
2024-06-26 00:08:10 +0100

    fff

--- config/db.js
+++ config/db.js
@@ -6,8 +6,14 @@ dotenv.config();
 const { MONGO_URI } = process.env;
 
 export const connectDB = async () => {
-    await mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => console.log("DB Connected"));
+    if (!MONGO_URI) {
+        throw new Error('MONGO_URI is not defined');
+    }
+
+    await mongoose.connect(MONGO_URI)
+        .then(() => console.log("DB Connected"))
+        .catch((err) => {
+            console.error('Error connecting to the database', err);
+            process.exit(1);
+        });
 };
